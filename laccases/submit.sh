39d40c5ec7945b1331e66f72fbb04ccc27597196
Mary Riley <myriley@ucdavis.edu>
2026-01-27 16:07:11 -0800

    Add files via upload

---
+++ laccases/submit.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --job-name=docking
+#SBATCH --output=logs/out_%A_%a.out 
+#SBATCH --cpus-per-task=1
+#SBATCH --array=1-100
+#SBATCH --partition=low
+#SBATCH --time=72:00:00
+#SBATCH --mem=10G
+
+/quobyte/jbsiegelgrp/software/Rosetta_314/rosetta/main/source/bin/rosetta_scripts.static.linuxgccrelease -database /quobyte/jbsiegelgrp/software/Rosetta_314/rosetta/main/database @flags -user_tag $SLURM_ARRAY_TASK_ID -out:suffix $SLURM_ARRAY_TASK_ID -out:path:all ./results
